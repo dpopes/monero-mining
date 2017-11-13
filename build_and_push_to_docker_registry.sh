e13a97a1c6394f0f096d8a2c5d5ff7ae22f17aee
Daniel Popescu <danielpops@gmail.com>
2017-11-13 00:15:02 -0800

    do both

--- build_and_push_to_docker_registry.sh
+++ build_and_push_to_docker_registry.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
 
-#docker run -itd -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_moneropool
-docker run --rm -it -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_xmrpooldotnet
+docker run -itd --name danielpops-moneropool -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_moneropool
+docker logs --follow danielpops-moneropool &
+docker run -itd --name danielpops-xmrpooldotnet -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_xmrpooldotnet
+docker logs --follow danielpops-xmrpooldotnet &
