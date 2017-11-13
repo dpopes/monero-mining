ec1b49b64fd0d89ac51614af505e21cfcf0a6998
Daniel Popescu <danielpops@gmail.com>
2017-11-13 00:00:44 -0800

    Initial travis and build script

---
+++ build_and_push_to_docker_registry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ev
+
+#docker run -itd -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_moneropool
+docker run -itd -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_xmrpooldotnet
