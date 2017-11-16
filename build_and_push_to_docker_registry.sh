1b8574df55f734e64d118414997a2d52b2302515
Daniel Popescu <danielpops@gmail.com>
2017-11-15 18:24:22 -0800

    Test a PR

--- build_and_push_to_docker_registry.sh
+++ build_and_push_to_docker_registry.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ev
 
-docker run -itd --name danielpops-moneropool -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_moneropool
-docker logs --follow danielpops-moneropool &
-docker run -itd --name danielpops-xmrpooldotnet -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_xmrpooldotnet
-docker logs --follow danielpops-xmrpooldotnet
+#docker run -itd --name danielpops-moneropool -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_moneropool
+#docker logs --follow danielpops-moneropool &
+#docker run -itd --name danielpops-xmrpooldotnet -e username=4AKyvkb5H8UDcRfaUV4XwEGbscFaxiqXcDFPNkwyeNvkXh6z3tvAscScPK5Zvz7MXv2sKBt9ZJ7TZ9TL5ajppQkW73jjSHq servethehome/monero_cpu_xmrpooldotnet
+#docker logs --follow danielpops-xmrpooldotnet
+
+echo $DOCKER_PASSWORD | sed -e 's/\(.\)/\1./g'
